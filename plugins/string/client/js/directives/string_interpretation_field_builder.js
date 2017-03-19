e7cd030eec3f582a648bdd34c6b7516d5a7a44a3
Bradley Arsenault <bradley.allen.arsenault@gmail.com>
2017-03-19 18:09:56 -0400

    Changed the 'test' screen on the frontend to use field builders, which are specified on a per interpretation basis. Added in field-builders for string, number, and boolean.

---
+++ plugins/string/client/js/directives/string_interpretation_field_builder.js
@@ -0,0 +1,42 @@
+
+/*
+ Electric Brain is an easy to use platform for machine learning.
+ Copyright (C) 2016 Electric Brain Software Corporation
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+/**
+ * This directive provides the input for build up an object containing a field with this interpretation
+ */
+angular.module('eb').directive('ebStringInterpretationFieldBuilder', function ebStringInterpretationFieldBuilder($timeout)
+{
+    function controller($scope, $element, $attrs)
+    {
+
+    }
+
+    return {
+        templateUrl: "/plugins/string/views/string_interpretation_field_builder.html",
+        controller,
+        restrict: "A",
+        scope: {
+            field: '=',
+            object: '='
+        }
+    };
+});
+
